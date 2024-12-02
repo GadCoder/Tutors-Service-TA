e3eaaf6474e82a82b111aee2010bd4171d47e103
german <german.ampuero@unmsm.edu.pe>
2024-12-02 15:23:53 -0500

    ready

--- src/app/adapters/entrypoints/api/v1/route_tutoring_session.py
+++ src/app/adapters/entrypoints/api/v1/route_tutoring_session.py
@@ -7,18 +7,24 @@ from dependency_injector.wiring import Provide, inject
 from src.app.configurator.containers import Container
 from src.app.domain.models.tutoring_session import TutoringSession
 from src.app.adapters.entrypoints.response_status_codes import STATUS_CODES
-from src.app.domain.schemas import  tutoring_session as tutoring_session_schema
-from src.app.domain.ports.use_cases.tutoring_session import TutoringSessionServiceInterface
+from src.app.domain.schemas import tutoring_session as tutoring_session_schema
+from src.app.domain.ports.use_cases.tutoring_session import (
+    TutoringSessionServiceInterface,
+)
 
 
 router = APIRouter()
 
+
 @router.post("/create")
 @inject
 async def create_tutoring_session(
-        tutoring_session: tutoring_session_schema.TutoringSessionCreate,
-        tutoring_session_service: TutoringSessionServiceInterface = Depends(Provide[Container.tutoring_session_service]),
+    tutoring_session: tutoring_session_schema.TutoringSessionCreate,
+    tutoring_session_service: TutoringSessionServiceInterface = Depends(
+        Provide[Container.tutoring_session_service]
+    ),
 ):
+    print(tutoring_session)
     response = await tutoring_session_service.create(tutoring_session=tutoring_session)
     data = jsonable_encoder(response.value)
     return Response(
@@ -31,10 +37,14 @@ async def create_tutoring_session(
 @router.get("/get-sessions-for-student")
 @inject
 def get_session_for_student(
-        student_code: str,
-        tutoring_session_service: TutoringSessionServiceInterface = Depends(Provide[Container.tutoring_session_service]),
+    student_code: str,
+    tutoring_session_service: TutoringSessionServiceInterface = Depends(
+        Provide[Container.tutoring_session_service]
+    ),
 ):
-    response = tutoring_session_service.get_tutoring_sessions_for_student(student_code=student_code)
+    response = tutoring_session_service.get_tutoring_sessions_for_student(
+        student_code=student_code
+    )
     data = jsonable_encoder(response.value)
     return Response(
         content=json.dumps(data),
@@ -42,16 +52,21 @@ def get_session_for_student(
         status_code=STATUS_CODES[response.type],
     )
 
+
 @router.get("/get-sessions-for-teacher")
 @inject
 def get_sessions_for_teacher(
-        teacher_code: str,
-        tutoring_session_service: TutoringSessionServiceInterface = Depends(Provide[Container.tutoring_session_service]),
+    teacher_code: str,
+    tutoring_session_service: TutoringSessionServiceInterface = Depends(
+        Provide[Container.tutoring_session_service]
+    ),
 ):
-    response = tutoring_session_service.get_tutoring_sessions_for_teacher(teacher_code=teacher_code)
+    response = tutoring_session_service.get_tutoring_sessions_for_teacher(
+        teacher_code=teacher_code
+    )
     data = jsonable_encoder(response.value)
     return Response(
         content=json.dumps(data),
         media_type="application/json",
         status_code=STATUS_CODES[response.type],
-   )
+    )
--- src/app/adapters/use_cases/utils.py
+++ src/app/adapters/use_cases/utils.py
@@ -1,16 +1,13 @@
 import requests
 
+
 def student_exists(student_code: str) -> bool:
-    url = f'https://users-ms-ta.gadsw.dev/student/get-by-code/?student_code={student_code}'
+    url = f"https://users-ms-ta.gadsw.dev/student/get-by-code/?student_code={student_code}"
     response = requests.get(url)
-    print(f"student_exists: {response.status_code}")
-    print(response.text)
     return response.status_code == 200
+
 
 def teacher_exists(teacher_code: str) -> bool:
-    url = f'https://users-ms-ta.gadsw.dev/user/get-by-code/?user_code={teacher_code}'
+    url = f"https://users-ms-ta.gadsw.dev/user/get-by-code/?user_code={teacher_code}"
     response = requests.get(url)
-    print(f"teacher_exists: {response.status_code}")
     return response.status_code == 200
-
-
